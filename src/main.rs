43774b7d79162d5a61c8a1c0a5bf0da74b9eadca
Andrew Soden <soden.andrew@gmail.com>
2024-08-21 23:54:38 -0500

    Spawn bullet

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::f32::consts::PI;
 use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
 use bevy::math::{vec2, vec3};
 use bevy::prelude::*;
+use bevy::time::Stopwatch;
 use bevy::window::PrimaryWindow;
 
 // Window
@@ -23,6 +24,9 @@ const PLAYER_SPEED: f32 = 2.0;
 // Colors
 const BG_COLOR: (f32, f32, f32) = (0.773, 0.8, 0.723);
 
+// Wepaon
+const BULLET_SPAWN_INTERVAL: f32 = 0.1;
+
 // Resources
 #[derive(Resource)]
 struct GlobalTextureAtlasHandle(Option<Handle<TextureAtlasLayout>>);
@@ -39,6 +43,11 @@ struct Player;
 
 #[derive(Component)]
 struct Weapon;
+
+#[derive(Component)]
+struct WeaponTimer(Stopwatch);
+#[derive(Component)]
+struct Bullet;
 
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
 enum GameState {
@@ -79,6 +88,7 @@ fn main() {
                 handle_player_input,
                 update_weapon_transform,
                 update_cursor_position,
+                handle_weapon_input,
             )
                 .run_if(in_state(GameState::InGame)),
         )
@@ -142,6 +152,7 @@ fn init_world(
             index: 2,
         },
         Weapon,
+        WeaponTimer(Stopwatch::new()),
     ));
 
     next_state.set(GameState::InGame);
@@ -198,6 +209,45 @@ fn handle_player_input(
     transform.translation += vec3(delta.x, delta.y, 0.0) * PLAYER_SPEED;
 }
 
+fn handle_weapon_input(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut weapon_query: Query<(&Transform, &mut WeaponTimer), With<Weapon>>,
+    mouse_button_input: Res<ButtonInput<MouseButton>>,
+    texture_atlas: Res<GlobalTextureAtlasHandle>,
+    image_handle: Res<GlobalSpriteSheetHandle>,
+) {
+    if weapon_query.is_empty() {
+        return;
+    }
+
+    let (weapon_transform, mut weapon_timer) = weapon_query.single_mut();
+    let weapon_pos = weapon_transform.translation.truncate();
+    weapon_timer.0.tick(time.delta());
+
+    if !mouse_button_input.pressed(MouseButton::Left) {
+        return;
+    }
+
+    if weapon_timer.0.elapsed_secs() >= BULLET_SPAWN_INTERVAL {
+        weapon_timer.0.reset();
+
+        commands.spawn((
+            SpriteBundle {
+                texture: image_handle.0.clone().unwrap(),
+                transform: Transform::from_translation(vec3(weapon_pos.x, weapon_pos.y, 1.0))
+                    .with_scale(Vec3::splat(SPRITE_SCALE_FACTOR)),
+                ..default()
+            },
+            TextureAtlas {
+                layout: texture_atlas.0.clone().unwrap(),
+                index: 3,
+            },
+            Bullet,
+        ));
+    }
+}
+
 fn update_cursor_position(
     mut cursor_pos: ResMut<CursorPosition>,
     window_query: Query<&Window, With<PrimaryWindow>>,
@@ -236,11 +286,15 @@ fn update_weapon_transform(
     weapon_transform.rotation = Quat::from_rotation_z(angle);
 
     let offset = 20.0;
-    let new_gun_pos = vec2(
+    let new_weapon_pos = vec2(
         player_pos.x + offset * angle.cos() - 5.0,
         player_pos.y + offset * angle.sin() - 10.0,
     );
 
-    weapon_transform.translation = vec3(new_gun_pos.x, new_gun_pos.y, weapon_transform.translation.z);
+    weapon_transform.translation = vec3(
+        new_weapon_pos.x,
+        new_weapon_pos.y,
+        weapon_transform.translation.z,
+    );
     weapon_transform.translation.z = 15.0;
 }
